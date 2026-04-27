91a0180271e5e73c4d515b188a4e3430438e04cc
Tobias Cadoná Marion <tobiascm87@gmail.com>
2026-04-27 08:42:49 -0300

    feat: add dark mode toggle component

--- apps/frontend/src/routes/app/route.tsx
+++ apps/frontend/src/routes/app/route.tsx
@@ -1,3 +1,4 @@
+import { ToogleDarkMode } from '@/components/toogle-dark-mode'
 import { bootstrapAuth } from '@/lib/auth/bootstrap-auth'
 import { authStore } from '@/lib/auth/store'
 import { Link, Outlet, createFileRoute, redirect } from '@tanstack/react-router'
@@ -31,16 +32,21 @@ function AppLayout() {
   }
 
   return (
-    <main className='flex-1 p-6 flex flex-col gap-16'>
-      <div>
-        This is the app layout
-        <h1>Hello, {user.name}</h1>
-        <span>{user.email}</span>
-        <Link to='/logout' className='block border border-red-500 p-8 w-fit'>
-          Logout
-        </Link>
-      </div>
-      <Outlet />
-    </main>
+    <>
+      <header>
+        <ToogleDarkMode />
+      </header>
+      <main className='flex-1 p-6 flex flex-col gap-16'>
+        <div>
+          This is the app layout
+          <h1>Hello, {user.name}</h1>
+          <span>{user.email}</span>
+          <Link to='/logout' className='block border border-red-500 p-8 w-fit'>
+            Logout
+          </Link>
+        </div>
+        <Outlet />
+      </main>
+    </>
   )
 }
