e99e2548cf0475618ee3e0865871490e161f4a87
fprossoni <fpasinato84@gmail.com>
2026-06-04 11:21:47 -0300

    Controller updated with examples and correction of routes

--- apps/backend/src/rooms/rooms.controller.ts
+++ apps/backend/src/rooms/rooms.controller.ts
@@ -14,15 +14,32 @@ import { RoomsService } from './rooms.service';
 import { CreateRoomDTO } from './dto/CreateRoom';
 import { UpdateRoomDTO } from './dto/UpdateRoom';
 
-@Controller()
+@Controller('rooms')
 export class RoomsController {
   constructor(private readonly roomsService: RoomsService) {}
 
-  @Post('rooms/new')
+  @Post('new')
   @Auth()
   @ApiOperation({ summary: 'Create new room' })
   @ApiBody({ type: CreateRoomDTO })
-  @ApiResponse({ status: 201, description: 'Created successfully' })
+  @ApiResponse({
+    status: 201, 
+    description: 'Created successfully',
+    schema: {
+      example: 
+        {
+          "id": "65e6f727-c7ff-4c5c-9592-cbfcb39cc067",
+          "number": "210",
+          "building": "43421",
+          "floor": 2,
+          "createdBy": "709ef1a9-a2b3-4c8a-a36a-eeb1c6b2367a",
+          "isArchived": false,
+          "archivedAt": null,
+          "createdAt": "2026-06-04T13:59:41.589Z",
+          "updatedAt": "2026-06-04T13:59:41.589Z"
+        }
+    }
+  })
   @ApiResponse({ status: 400, description: 'Validation error' })
   @ApiResponse({ status: 401, description: 'Unauthorized' })
   @ApiResponse({ status: 403, description: 'Forbidden - Insufficient permissions' })
@@ -31,31 +48,92 @@ export class RoomsController {
     return this.roomsService.create(body, user);
   }
 
-  @Get('rooms')
+  @Get()
   @Auth()
   @ApiOperation({ summary: 'Find all active rooms (admin only)' })
-  @ApiResponse({ status: 200, description: 'Authorized access' })
+  @ApiResponse({ 
+    status: 200, 
+    description: 'Authorized access',
+    schema: {
+      example: [
+        {
+          "id": "65e6f727-c7ff-4c5c-9592-cbfcb39cc067",
+          "number": "210",
+          "building": "43421",
+          "floor": 2,
+          "createdBy": "709ef1a9-a2b3-4c8a-a36a-eeb1c6b2367a",
+          "isArchived": false,
+          "archivedAt": null,
+          "createdAt": "2026-06-04T13:59:41.589Z",
+          "updatedAt": "2026-06-04T13:59:41.589Z"
+        },
+        {
+          "id": "5969c6ec-b783-42a2-8714-7fad314221bc",
+          "number": "214",
+          "building": "43421",
+          "floor": 2,
+          "createdBy": "0e4a003e-8aa0-4480-beb3-19abe2ca096b",
+          "isArchived": false,
+          "archivedAt": null,
+          "createdAt": "2026-06-03T23:25:40.265Z",
+          "updatedAt": "2026-06-03T23:25:40.265Z"
+        }
+      ]
+    }
+  })
   @ApiResponse({ status: 401, description: 'Unauthorized' })
   @ApiResponse({ status: 500, description: 'Internal Server Error' })
   async getAllRooms(@CurrentUser() user: User) {
     return this.roomsService.findAllRooms(user);
   }
 
-  @Get('rooms/:id')
+  @Get(':id')
   @Auth()
   @ApiOperation({ summary: 'Find room by id' })
   @ApiParam({ name: 'id', type: 'string', format: 'uuid' })
-  @ApiResponse({ status: 200, description: 'Room found' })
+  @ApiResponse({ 
+    status: 200, 
+    description: 'Room found',
+    schema: {
+      example: {
+        "id": "5969c6ec-b783-42a2-8714-7fad314221bc",
+        "number": "214",
+        "building": "43421",
+        "floor": 2,
+        "createdBy": "0e4a003e-8aa0-4480-beb3-19abe2ca096b",
+        "isArchived": false,
+        "archivedAt": null,
+        "createdAt": "2026-06-03T23:25:40.265Z",
+        "updatedAt": "2026-06-03T23:25:40.265Z"
+      }
+    }
+  })
   @ApiResponse({ status: 401, description: 'Unauthorized' })
   @ApiResponse({ status: 404, description: 'Room not found' })
   async getRoomById(@Param('id') id: string, @CurrentUser() user: User) {
     return this.roomsService.findRoomById(id, user);
   }
 
-  @Patch('rooms/:id')
+  @Patch(':id')
   @Auth()
   @ApiOperation({ summary: 'Update room' })
-  @ApiResponse({ status: 200, description: 'Room updated successfully' })
+  @ApiResponse({ 
+    status: 200, 
+    description: 'Room updated successfully',
+    schema: {
+      example: {
+        "id": "5969c6ec-b783-42a2-8714-7fad314221bc",
+        "number": "216",
+        "building": "43421",
+        "floor": 2,
+        "createdBy": "0e4a003e-8aa0-4480-beb3-19abe2ca096b",
+        "isArchived": false,
+        "archivedAt": null,
+        "createdAt": "2026-06-03T23:25:40.265Z",
+        "updatedAt": "2026-06-04T14:09:24.074Z"
+      }
+    }
+  })
   @ApiResponse({ status: 400, description: 'Invalid request body' })
   @ApiResponse({ status: 401, description: 'Unauthorized' })
   @ApiResponse({ status: 403, description: 'Forbidden - Insufficient permissions' })
@@ -64,10 +142,26 @@ export class RoomsController {
     return this.roomsService.update(id, data, user);
   }
   
-  @Delete('rooms/:id')
+  @Delete(':id')
   @Auth()
   @ApiOperation({ summary: 'Archive room' })
-  @ApiResponse({ status: 200, description: 'Room archived successfully' })
+  @ApiResponse({ 
+    status: 200, 
+    description: 'Room archived successfully',
+    schema: {
+      example: {
+        "id": "5969c6ec-b783-42a2-8714-7fad314221bc",
+        "number": "216",
+        "building": "43421",
+        "floor": 2,
+        "createdBy": "0e4a003e-8aa0-4480-beb3-19abe2ca096b",
+        "isArchived": true,
+        "archivedAt": "2026-06-04T14:11:14.480Z",
+        "createdAt": "2026-06-03T23:25:40.265Z",
+        "updatedAt": "2026-06-04T14:11:14.486Z"
+      }
+    }
+  })
   @ApiResponse({ status: 401, description: 'Unauthorized' })
   @ApiResponse({ status: 403, description: 'Forbidden - Insufficient permissions' })
   @ApiResponse({ status: 404, description: 'Room not found' })
