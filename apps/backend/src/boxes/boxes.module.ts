744601300146947acacebcf05bafe8a937c86148
fprossoni <fpasinato84@gmail.com>
2026-06-11 20:25:11 -0300

    Boxes module implemented

--- apps/backend/src/boxes/boxes.module.ts
+++ apps/backend/src/boxes/boxes.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module'
 import { BoxesService } from './boxes.service';
 import { BoxesController } from './boxes.controller';
 
 @Module({
+  imports: [AuthModule],
   controllers: [BoxesController],
   providers: [BoxesService],
 })
