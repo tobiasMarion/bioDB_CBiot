e5f3522be6cf9afef8e6e1c31735a6bd51c6dd59
Felipe Rossoni <163370983+fprossoni@users.noreply.github.com>
2026-06-11 20:23:05 -0300

    Delete apps/backend/src/box directory

--- apps/backend/src/box/box.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BoxService } from './box.service';
-import { BoxController } from './box.controller';
-
-@Module({
-  controllers: [BoxController],
-  providers: [BoxService],
-})
-export class BoxModule {}
--- apps/backend/src/box/dto/UpdateBox.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBoxDTO } from './CreateBox';
-
-export class UpdateBoxDTO extends PartialType(CreateBoxDTO) {}
