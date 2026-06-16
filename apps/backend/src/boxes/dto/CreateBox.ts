b931bc65f0b2f64c61612397d6cded3cd3c5a0da
fprossoni <fpasinato84@gmail.com>
2026-06-15 22:58:32 -0300

    DTOs updated to remove groupId

--- apps/backend/src/boxes/dto/CreateBox.ts
+++ apps/backend/src/boxes/dto/CreateBox.ts
@@ -1,5 +1,5 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
-import { IsNotEmpty, IsUUID, IsOptional, IsString, MinLength } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty, IsString, MinLength } from 'class-validator'
 
 export class CreateBoxDTO {
   @ApiProperty({ example: "Felipe Rossoni's Box" })
@@ -7,9 +7,4 @@ export class CreateBoxDTO {
   @IsNotEmpty()
   @MinLength(1)
   label!: string
-
-  @ApiPropertyOptional({ example: '550e8400-e29b-41d4-a716-446655440000' })
-  @IsUUID()
-  @IsOptional()
-  groupId?: string
 }
--- apps/backend/src/boxes/dto/UpdateBox.ts
+++ apps/backend/src/boxes/dto/UpdateBox.ts
@@ -1,4 +1,10 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBoxDTO } from './CreateBox';
+import { ApiPropertyOptional } from '@nestjs/swagger'
+import { IsOptional, IsString, MinLength } from 'class-validator'
 
-export class UpdateBoxDTO extends PartialType(CreateBoxDTO) {}
+export class UpdateBoxDTO {
+  @ApiPropertyOptional({ example: "Felipe Rossoni's Box" })
+  @IsString()
+  @IsOptional()
+  @MinLength(1)
+  label?: string
+}
